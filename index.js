b349f22950ef6cf43e2c88d863177332b868d413
kaulmesanyam <kaulsanyam@gmail.com>
2023-07-30 16:02:22 +0530

    created /todos get route

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html")
 })
 
-
+app.get('/todos', (req, res) => {
+    fs.readFile('todos.txt', 'utf8', (err, data) => {
+       if(err) {
+        new Error("Error occured while reading file");
+       } else {
+        res.json(JSON.parse(data));
+       }
+    } )
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
